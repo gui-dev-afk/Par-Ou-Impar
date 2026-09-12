52d4f83574eab89aa5541f014f7c71b13b177739
Guilherme de Oliveira Santos <gui09106@gmail.com>
2026-09-11 23:14:44 -0300

    feat: adiciona verificador de par ou impar com validacao de entrada

--- par.java
+++ par.java
@@ -5,14 +5,49 @@ public class par {
 
         Scanner resp = new Scanner(System.in);
 
-        int numero;
+        int numero = 0, lixo2;
+        String lixo;
+        char outro;
 
-        System.out.print("Digite um Numero: ");
-        numero = resp.nextInt();
+        do {
+            do {
 
-        if (numero % 2 == 0)
-            System.out.println("Par");
-        else
-            System.out.println("Impar");
+                System.out.print("Digite um Numero: ");
+                if (resp.hasNextInt()) {
+                    numero = resp.nextInt();
+                    lixo = "b";
+                } else {
+                    System.out.println("DIGITE UM NUMERO VALIDO!");
+                    System.out.println();
+                    resp.next();
+                    lixo = "a";
+                }
+
+            } while (lixo.equals("a"));
+
+            if (numero % 2 == 0)
+                System.out.println("Par");
+            else
+                System.out.println("Impar");
+
+            do {
+
+                System.out.println("Deseja verificar outro numero? (S/N)");
+                outro = resp.next().charAt(0);
+
+                if(outro != 'S' && outro != 's' && outro != 'N' && outro != 'n')
+                {
+                    System.out.println("COLOQUE S OU N");
+                    lixo2 = 1;
+
+                }
+                else
+                    lixo2 = 2;
+
+            }while(lixo2 == 1);
+
+
+
+        }while(outro == 'S' || outro == 's');
     }
 }
